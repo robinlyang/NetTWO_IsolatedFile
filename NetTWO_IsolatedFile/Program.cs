0e74999452ed26afb03cf3ac40189f4701e87f0c
robinlyang <robiny@gmail.com>
2016-11-23 18:53:47 -0700

    Initial Project

--- NetTWO_IsolatedFile/Program.cs
+++ NetTWO_IsolatedFile/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.IO.IsolatedStorage; //created files for each user
 
 namespace NetTWO_IsolatedFile
 {
@@ -10,6 +12,25 @@ namespace NetTWO_IsolatedFile
     {
         static void Main(string[] args)
         {
+            IsolatedStorageFileStream f = new IsolatedStorageFileStream("abc", FileMode.Create);
+            //do NOT give a directory for file name
+            StreamWriter w = new StreamWriter(f);
+            w.WriteLine("password");
+            w.WriteLine("menu");
+            w.Close();
+            f.Close();
+
+            //read back
+            f = new IsolatedStorageFileStream("abc", FileMode.Open);
+            StreamReader r = new StreamReader(f);
+            string data = r.ReadLine();
+            Console.WriteLine(data);
+            r.Close();
+            f.Close();
+
+            Console.ReadLine();
+
+            
         }
     }
 }
